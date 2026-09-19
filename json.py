8ca125a09a3f8daf122fe686de4853396ebb824d
Abhinav. S. Nair <165151802+Sarish002@users.noreply.github.com>
2026-09-19 23:55:35 +0530

    Delete json.py

--- json.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import write
-
-mols = os.listdir("Images")
-with open("json.json", "w") as json:
-    molstr = str(mols)
-    molstrlist = molstr.split(", ")
-    molslist = "".join([i + ",\n " for i in molstrlist]).strip(",")
-    json.write(molslist)
